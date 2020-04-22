018a45e05a1a5463a2a172cc150a27f2ade285b9
KonstantinKlychkov <klychkovks@gmail.com>
2020-04-22 22:51:50 +0300

    Добавлена кнопка "Старт" Теперьигра начинается после нажатия кнопки "Старт", как хотел заказчик.

--- js/game.js
+++ js/game.js
@@ -9,11 +9,13 @@ var missedFieldId = '#slot-11';
 var missedList = [];
 
 function getTimestamp() {
+  //определим текущее время
   let d = new Date();
   return d.getTime();
 }
 
 function randomDivId() {
+  // определим случайные координаты целевой ячейки
   let d = Math.floor(Math.random() * 6) + 1;
   let n = Math.floor(Math.random() * 6) + 1;
   return `#slot-${d}${n}`;
@@ -24,50 +26,65 @@ function round() {
   if (hits >= maxHits) {
     endGame();
   } else {
+      // очистим игровое поле от целевой я чейки и промахов прошлого хода
       $(divSelector).removeClass("target");
       $(divSelector).text('');
-      missedList.forEach( element => $(element).removeClass("miss"));
+      missedList.forEach( element => $(element).removeClass("miss")); //очистим все ячейки с промахом
     }
 
+  // назначим новую ячейку целевой
   divSelector = randomDivId();
   $(divSelector).addClass("target");
   $(divSelector).text(hits+1);
   
   if (hits == 0) {
-    firstHitTime = getTimestamp();
+    firstHitTime = getTimestamp(); //запомним время начала игры для вычисления общей продолжительности
   }
 }
 
+function startGame() {
+  // переформатируем игровое поле, уберем кнопку "старт", добавим новую и игровое поле
+  $("#game-board").removeClass("d-none");
+  $("#button-reload").removeClass("d-none");
+  $("#button-start").addClass("d-none");
+}
+
 function endGame() {
-  // FIXME: спрятать игровое поле сначала
 
-  let totalPlayedMillis = getTimestamp() - firstHitTime;
-  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
-  $("#total-time-played").text(totalPlayedSeconds);
+  let totalPlayedMillis = getTimestamp() - firstHitTime; // посчитаем длительность игры
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(2);
+  $("#total-time-played").text(totalPlayedSeconds); // покажем дилтельность на экране
 
+  let successPercent = Number(hits * 100 / (hits + missed)).toPrecision(2); // посчитаем процент попаданий
+  $("#success-percent").text(successPercent); // покажем процент попаданий на экране
+  $("#progress-bar").attr("style", "width: " + successPercent + "%"); // закрасим процент попаданий на прогрессбаре
+
+  // переворматируем игровое поле
   $("#win-message").removeClass("d-none");
+  $("#game-board").addClass("d-none");
 }
 
 function handleClick(event) {
+  
   if ($(event.target).hasClass("target")) {
-    hits = hits + 1;
+    hits += 1;
     round();
   } else {
-    $(event.target).addClass("miss");
-    missedFieldId = "#" + $(event.target).attr("id");
-    missedList.push(missedFieldId);
-    missed +=1;
+      // если промахнулись, то покрасим ячейку в красный и добавим её id в лист, вдруг промхнёмся еще
+      $(event.target).addClass("miss");
+      missedFieldId = "#" + $(event.target).attr("id");
+      missedList.push(missedFieldId);
+      missed +=1;
     };
-  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
 }
 
 function init() {
-  // TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
   round();
-
+  $("#button-start").click(startGame);
   $(".game-field").click(handleClick);
   $("#button-reload").click(function() {
     location.reload();
+    startGame();
   });
 }
 
